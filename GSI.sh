7ab8d2ce868ecfd1fb5b2794631c90e0ce88f853
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-22 13:38:21 +0800

    Update GSI.sh

--- GSI.sh
+++ GSI.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+#更新软件列表及软件
+sudo apt update && sudo apt upgrade -y
+#获取工具 ErfanGSIs
+git clone --recurse-submodules https://github.com/erfanoabdi/ErfanGSIs.git
+cd ErfanGSIs
+#setup.sh
+sudo bash setup.sh
+
