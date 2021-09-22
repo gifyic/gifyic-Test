756a514e95934e091b34a436b955d731a61ef678
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-22 17:10:26 +0800

    Update GSI.sh

--- GSI.sh
+++ GSI.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "------------ Update software list and software ------------"
 sudo apt update && sudo apt upgrade -y
-wget https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py
 echo "------------ clone ErfanGSIs ------------"
 git clone --recurse-submodules https://github.com/erfanoabdi/ErfanGSIs.git
 cd ErfanGSIs
