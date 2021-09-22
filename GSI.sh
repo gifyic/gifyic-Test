26be6b667210e1d3d26db9ad2812b571dfc4e7be
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-22 21:50:06 +0800

    Update GSI.sh

--- GSI.sh
+++ GSI.sh
@@ -5,10 +5,10 @@ echo "------------ clone ErfanGSIs ------------"
 git clone --recurse-submodules https://github.com/erfanoabdi/ErfanGSIs.git
 cd ErfanGSIs
 echo "------------ Remove exit 1 from url2GSI.sh ------------"
-line='sed -n "/    exit 1/=" ./url2GSI.sh'
+line=`sed -n '/    exit 1/=' ./url2GSI.sh`
+sed -i "s/ || exit 1//g" ./url2GSI.sh
 sed -i "s/exit 1//g" ./url2GSI.sh
-sed -i "s/|| exit 1//g" ./url2GSI.sh
-sed -i ""$line"d" ./url2GSI.sh
+sed -i "$line d" ./url2GSI.sh
 echo "------------ Execute setup.sh ------------"
 sudo bash setup.sh
 
