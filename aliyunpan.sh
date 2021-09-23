f192ca016708d68fe2c727ae1ea12a00c9d4460f
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-23 16:46:53 +0800

    Update aliyunpan.sh

--- aliyunpan.sh
+++ aliyunpan.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/Hidove/aliyundrive-uploader.git
 cd aliyundrive-uploader
 pip install -r requirements.txt
-mv example.config.json config.json
+rm -rf example.config.json
 echo -e $ali > config.json
 chmod +x main.py
 python3 main.py
