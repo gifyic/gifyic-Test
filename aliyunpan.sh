d49b601db2208a2fa4fd6e949d375b8c210faf77
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-23 18:15:46 +0800

    Update aliyunpan.sh

--- aliyunpan.sh
+++ aliyunpan.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-git clone https://github.com/Hidove/aliyundrive-uploader.git
-cd aliyundrive-uploader
-pip install -r requirements.txt
-rm -rf example.config.json
-echo -e $ali > config.json
-chmod +x main.py
-python3 main.py
+wget https://github.com/Hidove/aliyundrive-uploader/releases/download/v2021.0919.2000/aliyundrive-uploader_linux_amd64.zip
+unzip aliyundrive-uploader_linux_amd64.zip
+chmod +x aliyundrive-uploader_linux_amd64
+cat>config.json<<EOF
+${{ secrets.ALI }}
+EOF
+./aliyundrive-uploader_linux_amd64
+
