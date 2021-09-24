0c0fb3cb119d5ae978be3df0e728e03e9384d24b
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-24 12:51:18 +0800

    Update aliyunpan.sh

--- aliyunpan.sh
+++ aliyunpan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $GITHUB_WORKSPACE
 pip install aliyunpan
-echo "refresh_token: $ali"  >  ~/.config/aliyunpan.yaml
+echo "refresh_token: dcfa219721d547b79e703e6147deb942"  >  ~/.config/aliyunpan.yaml
 aliyunpan-cli u $ROM_device Github_Actions
 
