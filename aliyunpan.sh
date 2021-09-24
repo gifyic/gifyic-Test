ad21da5b3e35657cbe5cc0c4b7fe454c0fff5ad4
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-24 11:49:27 +0800

    Update aliyunpan.sh

--- aliyunpan.sh
+++ aliyunpan.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-wget https://github.com/Hidove/aliyundrive-uploader/releases/download/v2021.0919.2000/aliyundrive-uploader_linux_amd64.zip
-unzip aliyundrive-uploader_linux_amd64.zip
-chmod +x aliyundrive-uploader_linux_amd64
-./aliyundrive-uploader_linux_amd64
+pip install aliyunpan
+echo "refresh_token: ${{ secrets.ALI }}"  >  ~/.config/aliyunpan.yaml
+aliyunpan-cli u  Github_Actions
 
