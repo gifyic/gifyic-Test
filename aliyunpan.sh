4c20698bab51b001622ae81f7c32170f9de3271e
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-24 13:09:13 +0800

    Update aliyunpan.sh

--- aliyunpan.sh
+++ aliyunpan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $GITHUB_WORKSPACE
 pip install aliyunpan
-echo "refresh_token: 'b0ceaeaec32c44b882c542ea5cb8100b'"  >  ~/.config/aliyunpan.yaml
+echo "refresh_token: '80cff98a9c65495b9ce65a1b34d7083c'"  >  ~/.config/aliyunpan.yaml
 aliyunpan-cli u $GITHUB_WORKSPACE/Redmi_Note7_Pro Github_Actions
 
