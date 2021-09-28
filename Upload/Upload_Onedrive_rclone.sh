69a46f553c3526b9c3cc0a16f3d0f960e126d8a2
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-28 17:22:51 +0800

    Update Upload_Onedrive_rclone.sh

--- Upload/Upload_Onedrive_rclone.sh
+++ Upload/Upload_Onedrive_rclone.sh
@@ -3,4 +3,4 @@ cd $GITHUB_WORKSPACE
 curl https://rclone.org/install.sh | sudo bash
 mkdir -p ~/.config/rclone/
 unzip -P ${{ secrets.PASSWD }} rclone.zip -d ~/.config/rclone/
-rclone copy  od:
+rclone copy "$ROM_device" od:Github_Actions/"$ROM_device"
