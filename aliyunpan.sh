624de9f913adcd5a081268ba32db324c51a6bbff
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-28 09:34:44 +0800

    Delete aliyunpan.sh

--- aliyunpan.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd $GITHUB_WORKSPACE
-pip install aliyunpan
-echo "refresh_token: '80cff98a9c65495b9ce65a1b34d7083c'"  >  ~/.config/aliyunpan.yaml
-aliyunpan-cli u $GITHUB_WORKSPACE/Redmi_Note7_Pro Github_Actions
-
