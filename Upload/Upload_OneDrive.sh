d2b243cd2a55a8d20f5cab6ad58af15ecfb9c7bc
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-28 12:28:21 +0800

    Update Upload_OneDrive.sh

--- Upload/Upload_OneDrive.sh
+++ Upload/Upload_OneDrive.sh
@@ -2,6 +2,6 @@
 cd $GITHUB_WORKSPACE
 wget https://github.com/gaowanliang/LightUploader/releases/download/v2.0.1/LightUploader_Linux_x86_64.tar.gz && tar -xzvf LightUploader_Linux_x86_64.tar.gz
 chmod +x LightUploader
-unzip -P ${{ secrets.PASSWD }} config.zip
+unzip -P "$OD" config.zip
  ./LightUploader -c actions@fuckod.onmicrosoft.com.json -t 32 -f "$ROM_device" -r "Github Actions"        
          
