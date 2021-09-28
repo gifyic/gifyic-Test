392930215c1c05624a27ff5ffca84410058718fa
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-28 17:20:28 +0800

    Update Upload_Onedrive_rclone.sh

--- Upload/Upload_Onedrive_rclone.sh
+++ Upload/Upload_Onedrive_rclone.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+cd $GITHUB_WORKSPACE
+curl https://rclone.org/install.sh | sudo bash
+mkdir -p ~/.config/rclone/
+unzip -P ${{ secrets.PASSWD }} rclone.zip -d ~/.config/rclone/
+rclone copy  od:
