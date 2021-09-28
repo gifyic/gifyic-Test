1fabb0f249ba4e9f1e0c4f8f5d03b96a77257cac
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-28 21:50:49 +0800

    Update GSI.sh

--- GSI.sh
+++ GSI.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-echo "------------ Update software list and software ------------"
 sudo apt update && sudo apt upgrade -y
-echo "------------ clone ErfanGSIs ------------"
 git clone --recurse-submodules https://github.com/erfanoabdi/ErfanGSIs.git
 cd ErfanGSIs
-echo "------------ Remove exit 1 from url2GSI.sh ------------"
 line=`sed -n '/    exit 1/=' ./url2GSI.sh`
+#Remove Exit
 sed -i "s/ || exit 1//g" ./url2GSI.sh
 sed -i "s/exit 1//g" ./url2GSI.sh
 sed -i "$line d" ./url2GSI.sh
-echo "------------ Execute setup.sh ------------"
 sudo bash setup.sh
 sudo bash ./url2GSI.sh --$GSI_TYPE $ROM_URL $OS_TYPE
 cd ..
