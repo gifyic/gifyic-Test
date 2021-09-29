461dea93d94c51b2b3809fc56517f77ee062318c
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-29 13:08:03 +0800

    Update Upload_wenshushu.sh

--- Upload/Upload_wenshushu.sh
+++ Upload/Upload_wenshushu.sh
@@ -3,4 +3,4 @@ cd $GITHUB_WORKSPACE
 git clone https://github.com/Aruelius/wenshushu.git && cd wenshushu
 pip install requests base58 pycryptodomex
 chmod +x wss.py
-python wss.py upload ../$ROM_device/$ROM_NAME
+python3 wss.py upload ../$ROM_device/$ROM_NAME
