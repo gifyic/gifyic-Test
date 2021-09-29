8592833150f0b41918e899e21a4fedf93dad6a9b
gifyic <81018600+gifyic@users.noreply.github.com>
2021-09-29 19:37:46 +0800

    Update Upload_cloud189.sh

--- Upload/Upload_cloud189.sh
+++ Upload/Upload_cloud189.sh
@@ -3,5 +3,5 @@ cd $GITHUB_WORKSPACE
 wget https://github.com/tickstep/cloudpan189-go/releases/download/v0.1.1/cloudpan189-go-v0.1.1-linux-amd64.zip -q
 unzip cloudpan189-go-v0.1.1-linux-amd64.zip
 cd cloudpan189-go-v0.1.1-linux-amd64
-cloudpan189-go login -username=`echo ${cloud189##* }` -password=`echo ${cloud189% *}`
+./cloudpan189-go login -username=`echo ${cloud189##* }` -password=`echo ${cloud189% *}`
 ./cloudpan189-go u ../"$ROM_device" /Github_Actions
